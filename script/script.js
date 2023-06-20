73062d2fde826cfa7b4df52544e3d970d01fcd19
TALEX1995 <tanase.alex@libero.it>
2023-06-20 15:20:32 +0200

    Added tasks and html

--- script/script.js
+++ script/script.js
@@ -3,7 +3,47 @@ console.log('JS OK', Vue)
 // Initialize app
 
 const app = Vue.createApp ({
-
+data: () => {
+    return {
+        tasks: [
+            {
+                id: 1,
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                id: 2,
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                id: 3,
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                id: 4,
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                id: 5,
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                id: 6,
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                id: 7,
+                text: 'Fare la spesa',
+                done: false
+            }
+        ]
+    }
+}
 })
 
 
