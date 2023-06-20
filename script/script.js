7a5928d3f54111b39317d6cb9f3685b0c3235eb6
TALEX1995 <tanase.alex@libero.it>
2023-06-20 16:22:04 +0200

    V for

--- script/script.js
+++ script/script.js
@@ -3,48 +3,54 @@ console.log('JS OK', Vue)
 // Initialize app
 
 const app = Vue.createApp ({
-data: () => {
-    return {
-        tasks: [
-            {
-                id: 1,
-                text: 'Fare la spesa',
-                done: false
-            },
-            {
-                id: 2,
-                text: 'Fare la spesa',
-                done: false
-            },
-            {
-                id: 3,
-                text: 'Fare la spesa',
-                done: false
-            },
-            {
-                id: 4,
-                text: 'Fare la spesa',
-                done: false
-            },
-            {
-                id: 5,
-                text: 'Fare la spesa',
-                done: false
-            },
-            {
-                id: 6,
-                text: 'Fare la spesa',
-                done: false
-            },
-            {
-                id: 7,
-                text: 'Fare la spesa',
-                done: false
-            }
-        ]
+    data: () => {
+        return {
+            tasks: [
+                {
+                    id: 1,
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    id: 2,
+                    text: 'Consegnare il progetto',
+                    done: false
+                },
+                {
+                    id: 3,
+                    text: 'Pulire',
+                    done: false
+                },
+                {
+                    id: 4,
+                    text: 'Studiare',
+                    done: false
+                },
+                {
+                    id: 5,
+                    text: 'Andare alla partita di calcetto',
+                    done: false
+                },
+                {
+                    id: 6,
+                    text: 'Andare a prendere i bambini a scuola',
+                    done: false
+                },
+                {
+                    id: 7,
+                    text: 'Preparare la cena',
+                    done: false
+                }
+            ]
+        }
+    },
+
+    methods: {
+        
     }
-}
-})
+
+    
+});
 
 
 // Mount app into DOM
